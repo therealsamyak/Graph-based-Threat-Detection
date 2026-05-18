6fd9111d0572d5706ee14bcb063e6cc3357bf32f
Samyak Kakatur <therealsamyak@gmail.com>
2026-05-17 19:05:11 -0700

    fixed linter warnings

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,12 +1,11 @@
 from src.data.lanl import (
-    AUTH_COLUMNS,
-    AUTH_NUMERIC,
-    FLOW_COLUMNS,
-    FLOW_NUMERIC,
-    REDTEAM_COLUMNS,
-    stream_gz_lines,
-    load_redteam,
-    build_window_intervals,
-    time_in_any_window,
+    AUTH_COLUMNS as AUTH_COLUMNS,
+    AUTH_NUMERIC as AUTH_NUMERIC,
+    FLOW_COLUMNS as FLOW_COLUMNS,
+    FLOW_NUMERIC as FLOW_NUMERIC,
+    REDTEAM_COLUMNS as REDTEAM_COLUMNS,
+    stream_gz_lines as stream_gz_lines,
+    load_redteam as load_redteam,
+    build_window_intervals as build_window_intervals,
+    time_in_any_window as time_in_any_window,
 )
-
